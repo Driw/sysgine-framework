9ec8d4ae9c4100298c16fe93e65b2d09f77b19f3
Driw <driwundernight@gmail.com>
2022-03-20 04:48:26 -0300

    Find Properties Annotated (SF-11) - Added function findPropertiesAnnotated() that list declared member properties annotated. - Added function findConstructorPropertiesAnnotated() that list constructor parameters annotated. - Added function findCompanionPropertiesAnnotated() that list companion object declared member properties annotated.

--- sysgine-framework-reflection/src/main/kotlin/br/com/driw/sysgineframework/reflection/AnnotationReflection.kt
+++ sysgine-framework-reflection/src/main/kotlin/br/com/driw/sysgineframework/reflection/AnnotationReflection.kt
@@ -3,7 +3,15 @@ package br.com.driw.sysgineframework.reflection
 import br.com.driw.sysgine.common.utils.has
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty1
 import kotlin.reflect.full.allSuperclasses
+import kotlin.reflect.full.companionObject
+import kotlin.reflect.full.declaredFunctions
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.hasAnnotation
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.kotlinFunction
 
 inline fun <reified A : Annotation> KClass<A>.findTypesAnnotated(): AnyKClassList =
@@ -17,10 +25,35 @@ inline fun <reified A : Annotation> KClass<A>.findInheritancesAnnotated(target: 
 		?: target.allSuperclasses
 			.firstOrNull { it.annotations.hasAnnotation<A>() }
 
+inline fun <reified A : Annotation> KClass<A>.findPropertiesAnnotated(target: AnyKClass): List<KProperty<*>> =
+	target.declaredMemberProperties
+		.filter { declaredProperty -> declaredProperty.annotations.hasAnnotation<A>() }
+
+inline fun <reified A : Annotation> KClass<A>.findConstructorPropertiesAnnotated(target: AnyKClass): List<KParameter> =
+	target.constructors.first()
+		.parameters
+		.filter { declaredProperty -> declaredProperty.annotations.hasAnnotation<A>() }
+
+inline fun <reified A : Annotation> KClass<A>.findCompanionPropertiesAnnotated(target: AnyKClass): List<KProperty1<out Any, *>> =
+	target.companionObject?.let {
+		it.memberProperties
+			.filter { declaredProperty -> declaredProperty.annotations.hasAnnotation<A>() }
+	} ?: emptyList()
+
 inline fun <reified A : Annotation> KClass<A>.findFunctionsAnnotated(): List<KFunction<*>> =
 	ReflectionProvider.reflection()
 		.getMethodsAnnotatedWith(this.java)
 		.map { it.kotlinFunction!! }
+
+inline fun <reified A : Annotation> KClass<A>.findFunctionsAnnotated(target: AnyKClass): List<KFunction<*>> =
+	target.declaredFunctions
+		.filter { declaredFunction -> declaredFunction.hasAnnotation<A>() }
+
+inline fun <reified A : Annotation> KClass<A>.findCompanionFunctionsAnnotated(target: AnyKClass): List<KFunction<*>> =
+	target.companionObject?.let {
+		it.declaredFunctions
+			.filter { declaredFunction -> declaredFunction.hasAnnotation<A>() }
+	} ?: emptyList()
 
 inline fun <reified A : Annotation> List<Annotation>.hasAnnotation(): Boolean =
 	this.has { a -> a.annotationClass == A::class }
